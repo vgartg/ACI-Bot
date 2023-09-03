b7df555656a8aea3fc9f8c2f4a1b3f0bd2898664
Valery Gorbanev <115099825+vgartg@users.noreply.github.com>
2023-09-03 22:06:00 +0300

    Big Update
    A large commit involving an almost complete rewrite of the code in order to reduce the code and remove duplicate elements. The main function will be as short and clear as possible. The rest of the code will be arranged in auxiliary files for convenience.

---
+++ ACI-main.cs
@@ -0,0 +1,30 @@
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using UpdateACI;
+
+namespace TGBot {
+    class ACI {
+        public static void Main(string[] args) {
+            var Client = new TelegramBotClient("6642792498:AAH0FLLbrjzoTz6fyw1AG1q3fi87yfYiPs4");
+            Client.StartReceiving(Update, Error);
+            Console.ReadLine();
+        }
+        async static Task Update(ITelegramBotClient client, Update update, CancellationToken token) {
+            var message = update.Message;
+            if (message != null) {
+                if (!UpDate.users.Contains(message.Chat.Username)) {
+                    if (message.Text != UpDate.key) UpDate.SendMes(client, message, "Enter key to activate bot:" +
+                        "\n\nIf you see this message for the second time, then the key was entered incorrectly.");
+                    else {
+                        UpDate.users.Add(message.Chat.Username);
+                        await UpdateAfterLogin.UpdateAfLog(client, update, token, message);
+                    }
+                }
+                else {
+                    await UpdateAfterLogin.UpdateAfLog(client, update, token, message);
+                }
+            }
+        }
+        private static Task Error(ITelegramBotClient client, Exception e, CancellationToken token) => throw e;
+    }
+}
